6f44a7cbe0d76a2e58fb84d9e314863f28cd9c43
Giada Pintos <g.pintos@ag-tech.ch>
2023-03-07 15:09:52 +0100

    cambiato_login_checker

--- src/js/login_checker.js
+++ src/js/login_checker.js
@@ -1,57 +1,35 @@
-//dichiaro il mio form con utente e password
 const form = document.querySelector('form');
-if (typeof userData === 'undefined') {
-    var userData = undefined;
-}
-
-//errore aggiunto al form
-var errorLabel = document.createElement("label");
+let userData = null;
+const errorLabel = document.createElement("label");
 errorLabel.innerHTML = "";
 form.appendChild(errorLabel);
 
-//Cattura dati dal json
 fetch('/data.json')
     .then(response => response.json())
     .then(data => {
         form.addEventListener('submit', (event) => {
             event.preventDefault();
-
-            //faccio il get del first e lastname
             const firstName = document.querySelector('#firstName').value;
             const lastName = document.querySelector('#lastName').value;
-            checkUserExists(data, firstName, lastName);
-
-            //controllo se l'utente esiste
             if (checkUserExists(data, firstName, lastName)) {
                 errorLabel.innerHTML = "";
-
-                //controllo se taxista oppure cliente
                 if (userData.isTaxi) {
                     window.location = "../../pages/taxiUC.html";
-
-                }
-                else if (!userData.isTaxi) {
+                } else {
                     window.location = "../../pages/clientiUC.html";
-
                 }
-            }
-            else {
+            } else {
                 errorLabel.innerHTML = "Password o Nome utente errati.";
                 errorLabel.style.color = "red";
             }
         });
     });
 
-
-//Confronta dati 
 function checkUserExists(data, firstName, lastName) {
     const foundUser = data.users.find(user => user.firstName === firstName && user.lastName === lastName);
-    if (data.users.some(user => user.firstName === firstName && user.lastName === lastName)) {
-
-        //istanzio l'utente come SINGLETON per tutta la sessione
+    if (foundUser) {
         localStorage.setItem('userData', JSON.stringify(foundUser));
-        userData = JSON.parse(localStorage.getItem('userData'));
-
+        userData = foundUser;
         return true;
     }
     return false;
