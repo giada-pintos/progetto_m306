c993fb1a46074c5f45c836d7506dd48515d36921
Giada Pintos <g.pintos@ag-tech.ch>
2023-03-07 13:41:44 +0100

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,7 @@
 //menu apposito per il tipo di cliente che sei
 var link = "authentication/login.html";
 var account = "";
-if (typeof userData === null) {
+if (typeof userData === 'undefined') {
     account = "Login";
 }
 else if (userData.isTaxi) {
